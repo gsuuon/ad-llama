dc69fb2a7035f8ede424f05b1ceccb8fc288934d
Steven Sun <gsuuon@gmail.com>
2023-07-29 18:18:06 -0500

    build: simplify rollup and tsconfig

---
+++ rollup.config.mjs
@@ -0,0 +1,20 @@
+import { builtinModules } from 'module'
+
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'lib/index.js',
+    format: 'esm',
+    name: 'ad-llama',
+    exports: 'named',
+  },
+  plugins: [
+    nodeResolve({ browser: true }),
+    commonjs({ ignore: builtinModules }),
+    typescript()
+  ],
+};
