3391d51782fceef9deabe2322aeb1f195e7c3eb4
Steven Sun <gsuuon@gmail.com>
2023-08-02 02:47:33 -0500

    feat(example): LoadReport, style improvements

--- example/vite-demo/src/main.ts
+++ example/vite-demo/src/main.ts
@@ -3,24 +3,43 @@ import { ad, guessModelSpecFromPrebuiltId, loadModel } from 'ad-llama'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>ad-llama + vite HMR</h1>
+    <h1>
+      <a href='https://github.com/gsuuon/ad-llama'><span id='gh'></span></a>
+      ad-llama
+    </h1>
     <p id='system'></p>
-    <p id='preprompt'></p>
+    <span id='preprompt'></span><span id='prompt'></span>
+    <pre>
+      <code id='completion'>
+      </code>
+    </pre>
+    <div id='redo'></div>
     <pre>
       <code id='template'>
       </code>
     </pre>
-    <p id='text'></p>
+    <pre><code id='progress'></code></pre>
   </div>
 `
 
 if (import.meta.hot) { import.meta.hot.accept() }
 
-const gen = ad(await loadModel(guessModelSpecFromPrebuiltId('Llama-2-7b-chat-hf-q4f32_1')))
+const progressEl = document.querySelector('#progress')!
 
-const { template, a } = gen('You are a dungeon master.', 'Create an interesting character based on the Dungeons and Dragons universe.')
+const gen = ad(
+  await loadModel(
+    guessModelSpecFromPrebuiltId('Llama-2-7b-chat-hf-q4f32_1'),
+    undefined,
+    report => {
+      progressEl.textContent = JSON.stringify(report, null, 2)
+    }
+  )
+)
 
-const result = template`
+const generate = async () => {
+  const { template, a } = gen('You are a dungeon master.', 'Create an interesting character based on the Dungeons and Dragons universe.')
+
+  const result = template`
 {
   "description": "${(a('description', {maxTokens: 1000, stops: ['\n']}))}",
   "name": "${(a('name'))}",
@@ -45,22 +64,40 @@ const result = template`
 }
 `
 
-const textEl = document.querySelector('#text')!
+  const completionEl = document.querySelector('#completion')!
+  const promptEl = document.querySelector('#prompt')!
+  const redoEl = document.querySelector('#redo')!
 
-const text = await result.collect(partial => {
-  if (partial.type === 'template') {
-    document.querySelector('#template')!.textContent = partial.content
-    document.querySelector('#system')!.textContent = partial.system
-    document.querySelector('#preprompt')!.textContent = partial.preprompt ?? ''
-  } else {
-    const el = document.createElement('span')
-    el.textContent = partial.content
-    el.className = partial.type
-    textEl.appendChild(el)
+  const text = await result.collect(partial => {
+    if (partial.type === 'template') {
+      document.querySelector('#template')!.textContent = partial.content
+      document.querySelector('#system')!.textContent = partial.system
+      document.querySelector('#preprompt')!.textContent = partial.preprompt ?? ''
+    } else {
+      const el = document.createElement('span')
+      el.textContent = partial.content
+      el.className = partial.type
+      completionEl.appendChild(el)
+
+      if (partial.type === 'gen') {
+        promptEl.textContent = " Generate " + partial.prompt
+      }
+    }
+  })
+
+  if (!redoEl.firstChild) {
+    const button = document.createElement('button')
+    button.onclick = () => {
+      completionEl.textContent = ''
+      promptEl.textContent = ''
+      generate()
+    }
+    button.textContent = 'redo'
+    redoEl.appendChild(button)
   }
-})
 
-console.log(text)
+  console.log(text)
+  console.log(JSON.parse(text))
+}
 
-console.log(JSON.parse(text))
-
+generate()
