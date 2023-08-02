06adc1cb7edab461d440130c72a9f446af4d0f8e
Steven Sun <gsuuon@gmail.com>
2023-08-01 23:51:26 -0500

    feat(example): streaming completions

--- example/vite-demo/src/main.ts
+++ example/vite-demo/src/main.ts
@@ -1,6 +1,22 @@
 import './style.css'
 import { ad, guessModelSpecFromPrebuiltId, loadModel } from 'ad-llama'
 
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>ad-llama + vite HMR</h1>
+    <pre>
+      <code>
+{
+  "description": "\${(a('description'))}",
+  "weapon": "\${a('weapon')}",
+  "items": ["\${'three items in their possession'}]
+}
+      </code>
+    </pre>
+    <p id='text'></p>
+  </div>
+`
+
 if (import.meta.hot) { import.meta.hot.accept() }
 
 const gen = ad(await loadModel(guessModelSpecFromPrebuiltId('Llama-2-7b-chat-hf-q4f32_1')))
@@ -11,18 +27,20 @@ const result = template`
 {
   "description": "${(a('description'))}",
   "weapon": "${a('weapon')}",
-  "items": [${a('three items in their possession')}]
+  "items": ["${'three items in their possession'}]
 }
 `
 
-const text = await result.collect()
+const textEl = document.querySelector('#text')!
+
+const text = await result.collect(partial => {
+  const el = document.createElement('span')
+  el.textContent = partial.content
+  el.className = partial.type
+  textEl.appendChild(el)
+})
+
 console.log(text)
 
 console.log(JSON.parse(text))
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>ad-llama + vite HMR</h1>
-    <p>${text}</p>
-  </div>
-`
