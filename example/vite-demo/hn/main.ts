a41b930466d4056321357a360c8ef6b4bbc8c2a2
Steven Sun <gsuuon@gmail.com>
2023-08-04 12:22:07 -0500

    feat(example): hn better prompt template

--- example/vite-demo/hn/main.ts
+++ example/vite-demo/hn/main.ts
@@ -39,18 +39,26 @@ renderTemplate(app, async () => {
 
   const listing = await hnApiGetRandomWhosHiring()
 
-  const { template, a } = gen('You are a helpful assistant that catalogues job listings.', listing)
+  const { template, a, __ } = gen(
+    'You are a helpful assistant that catalogues job listings. For any requests for which there is not sufficient information based on the listing itself, fill the field with only "NA".',
+    `\nThe listing:\n"""\n${listing}\n"""\n`,
+    {
+      preword: 'What is'
+    }
+  )
 
   return template`{
   "company": {
-    "name": "${a('name')}",
-    "description": "${a('description')}",
+    "name": "${a('name for the company')}",
+    "description": "${a('description of the company and/or what it does. Fill with "NA" if no information is provided.')}",
     "sector": "${a('sector that this company operates in')}",
     "links": ["${a('list of additional links from the listing')}]
   },
   "role": {
-    "primary": "${a('role')}",
-    "additionalRoles": [${a('list of any additional roles mentioned, or ["NA"] if there are none', { stops: ["NA"]})}]
+    "primary": "${a('primary role')}",
+    "additionalRoles": [${a('list of any additional roles mentioned, or ["NA"] if there are none', {
+      stops: ["NA", "{"],
+    })}]
   },
   "salary": {
     "currency": "${a('currency label')}",
@@ -60,7 +68,9 @@ renderTemplate(app, async () => {
   },
   "skills": ["${a('list of skills')}],
   "remote": {
-    "allowed": ${a('boolean, true if remote is allowed else false', { stops: ['}',' ','\n']})},
+    "allowed": ${__('fill a javascript boolean, true if remote is allowed for this listing, else false. If the listing says ONSITE, that means remote is not allowed.', {
+      stops: ['}',' ','\n'],
+    })},
     "info": "${a('short description of their remote policy based on the listing or NA')}"
   }
 }`
