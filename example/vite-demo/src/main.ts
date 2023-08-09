08dbc9fa1282ac20cac4dd2d2f9e55a36c9f9134
Steven Sun <gsuuon@gmail.com>
2023-08-09 14:08:44 -0500

    feat(example): avoid, prefer

--- example/vite-demo/src/main.ts
+++ example/vite-demo/src/main.ts
@@ -1,13 +1,15 @@
 import './style.css'
 import { renderTemplate } from './renderTemplate'
-import { ad, guessModelSpecFromPrebuiltId, loadModel, TargetDevice } from 'ad-llama'
+import { ad, guessModelSpecFromPrebuiltId, loadModel, TargetDevice, chars, oneOf } from 'ad-llama'
 
 if (import.meta.hot) { import.meta.hot.accept() }
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
+const alsoToLowerCase = (x: string) => [x.toLowerCase(), x]
+
 renderTemplate(app, async () => {
-  const gen = ad(
+  const model =
     await loadModel(
       guessModelSpecFromPrebuiltId('Llama-2-7b-chat-hf-q4f32_1'),
       report => app.innerHTML = `<pre id='progress'><code>${JSON.stringify(report, null, 2)}</code></pre>`,
@@ -15,7 +17,8 @@ renderTemplate(app, async () => {
         ? TargetDevice.GPU // FIXME TargetDevice.CPU seems to be broken right now?
         : TargetDevice.CPU 
     )
-  )
+
+  const gen = ad(model)
 
   const { template, a } = gen(
     'You are a dungeon master.',
@@ -23,25 +26,33 @@ renderTemplate(app, async () => {
   )
 
   return template`{
-    "description": "${(a('clever description', {maxTokens: 1000, stops: ['\n']}))}",
-    "name": "${(a('name'))}",
-    "weapon": "${a('special weapon')}",
-    "items": [
-      {
-        "name": "${a('name')}",
-        "description": "${a('short description')}",
-        "type": "${a('type')}"
-      },
-      {
-        "name": "${a('name')}",
-        "description": "${a('short description')}",
-        "type": "${a('type')}"
-      },
-      {
-        "name": "${a('name')}",
-        "description": "${a('short description')}",
-        "type": "${a('type')}"
-      }
-    ]
-  }`
-})
+  "class": "${a('class', {
+    sampler: model.biases.avoid(oneOf(['Ranger', 'Rogue'].flatMap(alsoToLowerCase)), 100000)
+  })}",
+  "subclass": "${a('subclass')}",
+  "name": "${(a('name'))}",
+  "weapon": "${a('special weapon', {
+    sampler: model.biases.prefer(oneOf(['nun-chucks', 'beam cannon']), 10000)
+  })}",
+  "description": "${(a('clever description', {maxTokens: 1000, stops: ['\n']}))}",
+  "age": ${a('age', {
+    sampler: model.biases.prefer(chars.number, 100),
+  })},
+  "items": [
+    {
+      "name": "${a('name')}",
+      "description": "${a('short description')}",
+      "type": "${a('type')}"
+    },
+    {
+      "name": "${a('name')}",
+      "description": "${a('short description')}",
+      "type": "${a('type')}"
+    },
+    {
+      "name": "${a('name')}",
+      "description": "${a('short description')}",
+      "type": "${a('type')}"
+    }
+  ]
+}`})
