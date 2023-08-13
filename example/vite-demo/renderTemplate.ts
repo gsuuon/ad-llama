1f0a0ade2760d4628e58c73d42bbaae47c9e53c5
Steven Sun <gsuuon@gmail.com>
2023-08-13 14:29:42 -0500

    feat(example): renderTemplate returns completion

--- example/vite-demo/renderTemplate.ts
+++ example/vite-demo/renderTemplate.ts
@@ -1,6 +1,8 @@
+import { Template, StreamPartial } from 'ad-llama'
+
 const render = (el: HTMLElement, html: string) => el.innerHTML = html
 
-export const renderTemplate = async (root: HTMLElement, createTemplateCompletion: any) => {
+export const renderTemplate = async (root: HTMLElement, createTemplateCompletion: () => Promise<Template>) => {
   const renderRedoButton = () => {
     const redoButton = document.createElement('button')
     redoButton.onclick = () => renderTemplate(root, createTemplateCompletion)
@@ -8,64 +10,67 @@ export const renderTemplate = async (root: HTMLElement, createTemplateCompletion
     document.getElementById('controls')?.appendChild(redoButton)
   }
 
-  const renderPartial = (template: any) => {
+  const renderPartial = (template: Template) => {
     let staticPrompt = ''
     let completionEl : HTMLElement | null;
     let promptEl : HTMLElement | null;
 
-    return (partial: any) => {
-      if (partial.type === 'template') {
-        staticPrompt = partial.system + (partial.preprompt ? `\n${partial.preprompt}` : '')
+    return (partial: StreamPartial) => {
+      switch (partial.type) {
+        case 'template':
+          staticPrompt = partial.system + (partial.preprompt ? `\n${partial.preprompt}` : '')
 
-        render(
-          root,
-          `
-            <div id='prompt'><p>${staticPrompt}</p></div>
-            <details><summary>template</summary>
-              <pre><code>${partial.content}</code></pre>
-            </details>
-            <pre id='completion'><code></code></pre>
-            <div id='controls'><button id='cancel'>cancel</button></div>
-           `
-        )
+          render(
+            root,
+            `<div id='prompt'><p>${staticPrompt}</p></div>
+             <details><summary>template</summary>
+             <pre><code>${partial.content}</code></pre>
+             </details>
+             <pre id='completion'><code></code></pre>
+             <div id='controls'><button id='cancel'>cancel</button></div>`
+          )
 
-        const cancelButton = document.getElementById('cancel')!
-        cancelButton.onclick = () => {
-          template.model.cancel()
-          cancelButton.remove()
-          renderRedoButton()
-        }
-        cancelButton.style.display = 'block'
-      } else {
-        completionEl ??= document.getElementById('completion')
-        promptEl ??= document.getElementById('prompt')
+          const cancelButton = document.getElementById('cancel')!
+          cancelButton.onclick = () => {
+            template.model.cancel()
+            cancelButton.remove()
+            renderRedoButton()
+          }
+          cancelButton.style.display = 'block'
 
-        if (partial.type === 'ungen') {
+          completionEl = document.getElementById('completion')
+          promptEl = document.getElementById('prompt')
+
+          break
+        case 'ungen':
           for (let i = 1; i < partial.tokenCount; i++) {
             completionEl?.lastChild?.remove()
           }
-          return
-        }
+          break
+        case 'gen':
+        case 'lit':
+          const el = document.createElement('span')
+          el.textContent = partial.content
+          el.className = partial.type
+          completionEl?.appendChild(el)
 
-        const el = document.createElement('span')
-        el.textContent = partial.content
-        el.className = partial.type
-        completionEl?.appendChild(el)
-
-        if (partial.type === 'gen') {
-          render(promptEl!, `<p>${staticPrompt} ${partial.prompt}</p>`)
-        }
+          if (partial.type === 'gen') {
+            render(promptEl!, `<p>${staticPrompt} ${partial.prompt}</p>`)
+          }
+          break
       }
     }
   }
 
   const template = await createTemplateCompletion()
 
-  const completionResult = await template.collect(renderPartial(template))
+  const completionResult = await template.collect_refs(renderPartial(template))
 
   renderRedoButton()
   document.getElementById('cancel')?.remove()
 
   console.log(completionResult)
-  console.log(JSON.parse(completionResult))
+  console.log(JSON.parse(completionResult.completion))
+
+  return completionResult
 }
