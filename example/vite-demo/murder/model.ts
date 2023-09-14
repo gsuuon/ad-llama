f41b50c4b357bdc8b62f26eb3cb5d0d0009ad8e8
Steven Sun <gsuuon@gmail.com>
2023-09-14 13:35:46 -0500

    feat: Model types

--- example/vite-demo/murder/model.ts
+++ example/vite-demo/murder/model.ts
@@ -1,13 +1,86 @@
-export type Model = {
-  state: 'generate background'
-} | {
-  state: 'generate scene'
-} | {
-  state: 'generate character'
-} | {
-  state: 'conversation'
+type Background = {
+  setting: string
+  characterNames: string[]
 }
 
+type Character = {
+  name: string
+  role: string
+  description: string
+  secret: string
+  motivation: string
+  summary: string
+}
+
+type Message = {
+  character: string
+  content: string
+}
+
+type PlayerInput = {
+  kind: 'message'
+  content: string
+} | {
+  kind: 'action'
+  content: string
+}
+
+type Conversation = {
+  sceneSummary: string
+  messages: Message[]
+}
+
+type Scene = {
+  characters: Character[]
+  conversations: Conversation[]
+  description: string
+  summary: string
+}
+
+export type ModelScene = {
+  state: 'scene generate'
+  background: Background
+  characters: Character[]
+  scenes: Scene[]
+} | {
+  state: 'scene player input'
+  background: Background
+  characters: Character[]
+  scenes: Scene[]
+  scene: Scene
+}
+
+export type ModelConversation = {
+  state: 'conversation'
+  scene: Scene
+  background: Background
+  characters: Character[]
+  scenes: Scene[]
+} | {
+  state: 'conversation parse input'
+  playerInputRaw: string
+  scene: Scene
+  background: Background
+  characters: Character[]
+  scenes: Scene[]
+} | {
+  state: 'conversation response'
+  playerInput: PlayerInput
+  scene: Scene
+  background: Background
+  characters: Character[]
+  scenes: Scene[]
+}
+
+export type Model = {
+  state: 'background'
+} | {
+  state: 'character'
+  background: Background
+  characters: Character[]
+} | ModelScene
+  | ModelConversation
+
 export const initial: Model = {
-  state: 'generate background'
+  state: 'background'
 }
