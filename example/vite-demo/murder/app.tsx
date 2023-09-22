4b6f20f29a4bb220562ac75959fbee93c668fc45
Steven Sun <gsuuon@gmail.com>
2023-09-21 21:52:14 -0500

    feat: model with history

--- example/vite-demo/murder/app.tsx
+++ example/vite-demo/murder/app.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from 'solid-js'
+import { Show, createSignal } from 'solid-js'
 import { LoadedModel, ad } from 'ad-llama'
 
 import { View } from './views/type'
@@ -13,20 +13,33 @@ const App = <AppModel extends {state: string}>({
 }: {
   llm: LoadedModel
   view: View<AppModel>
-  initialAppModel: AppModel
+  initialAppModel: AppModel | AppModel[]
 }) => {
-  const [appModel, setAppModel] = createSignal<AppModel>(initialAppModel)
+  const [history, setHistory] = createSignal<AppModel[]>(
+    Array.isArray(initialAppModel) ? initialAppModel : [initialAppModel]
+  )
 
   const createTemplate = ad(llm)
+  ;(window as any).examine = () => console.log(history())
+
+  const update = (m: AppModel) => {
+    setHistory(history().concat([m]))
+  }
 
   return (
     // Using Dynamic here doesn't re-render consistently - I could carefully use signals in every view but
     // I think the expectation would be that an update triggers a re-render
     <>
-      {show(view, appModel().state, {
-          update: setAppModel,
+      <Show when={history().length > 1}>
+        <button onClick={() => {
+          llm.cancel()
+          setHistory(history().slice(0, -1))
+        }}>undo</button>
+      </Show>
+      {show(view, history()[history().length - 1].state, {
+          update,
           llm,
-          model: appModel,
+          model: () => history()[history().length - 1],
           ...createTemplate
         })
       }
