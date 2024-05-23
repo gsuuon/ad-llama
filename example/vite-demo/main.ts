5381e4560a605a2e2db41199ac42a3c152244ac3
Steven Sun <gsuuon@gmail.com>
2024-05-23 05:46:02 -0500

    wip: demo + accidental autoformat

--- example/vite-demo/main.ts
+++ example/vite-demo/main.ts
@@ -1,64 +1,72 @@
-import './style.css'
-import { renderTemplate } from './renderTemplate'
-import { ad, loadModel, TargetDevice, sample } from 'ad-llama'
+import "./style.css";
+import { renderTemplate } from "./renderTemplate";
+import { ad, loadModel, TargetDevice, sample } from "ad-llama";
 
-if (import.meta.hot) { import.meta.hot.accept() }
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
-const alsoToLowerCase = (x: string) => [x.toLowerCase(), x]
+const alsoToLowerCase = (x: string) => [x.toLowerCase(), x];
 
 renderTemplate(app, async () => {
-  const model =
-    await loadModel(
-      'Llama-2-7b-chat-hf-q4f16_1',
-      report => app.innerHTML = `<pre id='progress'><code>${JSON.stringify(report, null, 2)}</code></pre>`,
-      new URLSearchParams(window.location.search).get('cpu') === null
-        ? TargetDevice.GPU // TODO TargetDevice.CPU wont work until we have tvmjs wasm64 -- try to rebuild with emcc -sMEMORY64
-        : TargetDevice.CPU
-    )
+  const model = await loadModel(
+    {
+      modelLibWasmUrl:
+        "http://localhost:5173/lib/Llama-3-8B-Instruct-q3f16_1.wasm",
+      modelWeightsConfigUrl:
+        "https://huggingface.co/mlc-ai/Llama-3-8B-Instruct-q3f16_1-MLC/resolve/main/",
+    },
+    (report) =>
+      (app.innerHTML = `<pre id='progress'><code>${JSON.stringify(report, null, 2)}</code></pre>`),
+    new URLSearchParams(window.location.search).get("cpu") === null
+      ? TargetDevice.GPU // TODO TargetDevice.CPU wont work until we have tvmjs wasm64 -- try to rebuild with emcc -sMEMORY64
+      : TargetDevice.CPU,
+  );
 
-  const { context, a } = ad(model)
+  const { context, a } = ad(model);
 
   const dm = context(
-    'You are a dungeon master.',
-    'Create an interesting character based on the Dungeons and Dragons universe.'
-  )
+    "You are a dungeon master.",
+    "Create an interesting character based on the Dungeons and Dragons universe.",
+  );
 
-  const { bias } = model
-  const { oneOf, consistsOf, chars } = sample
+  const { bias } = model;
+  const { oneOf, consistsOf, chars } = sample;
 
   return dm`{
-  "class": "${a('primary class for the character', {
-    sampler: bias.reject(oneOf(['Ranger', 'Rogue'].flatMap(alsoToLowerCase)))
+  "class": "${a("primary class for the character", {
+    sampler: bias.reject(oneOf(["Ranger", "Rogue"].flatMap(alsoToLowerCase))),
   })}",
-  "subclass": "${a('subclass')}",
-  "name": "${(a('name', { sampler: bias.avoid(oneOf(['Eira', 'Zorvath', 'Kaelith']), 1.5) }))}",
-  "weapon": "${a('special weapon', { sampler: bias.prefer(oneOf(['Nun-chucks', 'Beam Cannon']), 10) })}",
-  "description": "${(a('clever description', {
+  "subclass": "${a("subclass")}",
+  "name": "${a("name", { sampler: bias.avoid(oneOf(["Eira", "Zorvath", "Kaelith"]), 1.5) })}",
+  "weapon": "${a("special weapon", { sampler: bias.prefer(oneOf(["Nun-chucks", "Beam Cannon"]), 10) })}",
+  "description": "${a("clever description", {
     maxTokens: 1000,
-    stops: ['\n'],
-    sampler: bias.avoid(consistsOf(['\n']), 1.2)
-  }))}",
-  "age": ${a('age', {
+    stops: ["\n"],
+    sampler: bias.avoid(consistsOf(["\n"]), 1.2),
+  })}",
+  "age": ${a("age", {
     sampler: bias.accept(chars.number),
-    maxTokens: 3
+    maxTokens: 3,
   })},
   "items": [
     {
-      "name": "${a('name')}",
-      "description": "${a('short description')}",
-      "type": "${a('type')}"
+      "name": "${a("name")}",
+      "description": "${a("short description")}",
+      "type": "${a("type")}"
     },
     {
-      "name": "${a('name')}",
-      "description": "${a('short description')}",
-      "type": "${a('type')}"
+      "name": "${a("name")}",
+      "description": "${a("short description")}",
+      "type": "${a("type")}"
     },
     {
-      "name": "${a('name')}",
-      "description": "${a('short description')}",
-      "type": "${a('type')}"
+      "name": "${a("name")}",
+      "description": "${a("short description")}",
+      "type": "${a("type")}"
     }
   ]
-}`})
+}`;
+});
